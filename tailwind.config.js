f92092036cfa6e18f4cd77811751246d67318053
Vasyl Yatsyshyn <beketgit@gmail.com>
2023-05-16 09:23:14 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        "./app/views/**/*.html.erb",
+        "./app/helpers/**/*.rb",
+        "./app/assets/stylesheets/**/*.css",
+        "./app/javascript/**/*.js",
+        './node_modules/flowbite/**/*.js'
+    ],
+    plugins: [require("flowbite/plugin")],
+    darkMode: 'media',
+};
